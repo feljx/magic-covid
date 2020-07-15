5080acb9d6126c652d97865b70903e7ab3ff2036
felix <11788526+flxsu@users.noreply.github.com>
2020-07-15 23:47:35 +0200

    upppp

---
+++ data-input/query.js
@@ -0,0 +1,19 @@
+const pg = require('pg')
+
+const URL =
+	'postgres://ootzddtw:51poFZTMbSQMH5Nw4dcWmF34-Z-FtsHh@rogue.db.elephantsql.com:5432/ootzddtw'
+
+const client = new pg.Client(URL)
+
+function query (query_string) {
+	client.connect(function (err) {
+		if (err) console.error(err)
+		client.query(query_string, function (err, result) {
+			if (err) console.error(err)
+			console.log(result)
+			client.end()
+		})
+	})
+}
+
+module.exports = query
