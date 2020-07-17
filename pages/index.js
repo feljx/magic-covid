3b156d5b0cd995d800ee2dd7cabf483a14920c05
Felix <11788526+flxsu@users.noreply.github.com>
2020-07-17 19:53:46 +0200

    cleanup

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-const data = require('../foo.json')
-
-function HomePage() {
-	const items = Object.entries(data).map(([k, v]) => <li key={k}>{v}</li>)
-
-    return <ul>{items}</ul>
-}
-
-export default HomePage
