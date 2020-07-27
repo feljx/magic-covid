d5d1e5983c795e7484f28e77cf2e945702482dd3
Felix <11788526+flxsu@users.noreply.github.com>
2020-07-27 19:23:52 +0200

    update db schema

--- src/data/sql/mods/country_names.sql
+++ src/data/sql/mods/country_names.sql
@@ -4,13 +4,21 @@ UPDATE
 SET
     name = REPLACE(name, '_', ' ');
 
--- change UK to GB geo code
+-- change Britain's geo code from UK to GB
 UPDATE
     countries
 SET
     geo_code = 'GB'
 WHERE
     geo_code = 'UK';
+
+-- change Greece's geo code from EL to GR
+UPDATE
+    countries
+SET
+    geo_code = 'GR'
+WHERE
+    geo_code = 'EL';
 
 -- format "international conveyance Japan" correctly
 UPDATE
